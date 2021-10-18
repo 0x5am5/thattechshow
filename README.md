c02f73d05cc5a297c24b67291e8860c0e0505816
Chris Addams <chris.addams@mail.com>
2021-10-18 22:31:26 +0100

    adding functionality to build episode feed

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/75f0cbf5-2366-4abd-8401-728051eb4648/deploy-status)](https://app.netlify.com/sites/thattechshow/deploys)
+
 # thattechshow
 
 Site is ran on Jekyll and built in Pinegrow
---
+++ feed.rb
@@ -0,0 +1,38 @@
+require 'rss'
+require 'rss/2.0'
+require 'open-uri'
+require 'fileutils'
+require 'safe_yaml'
+require 'httparty'
+require 'feedjira'
+
+url = 'https://s8hdp1sdz5.execute-api.eu-west-2.amazonaws.com/latest/proxy/'
+xml = HTTParty.get(url).body
+feed = Feedjira.parse(xml)
+
+feed.entries.each do |item|
+    formatted_date = item.published.strftime('%Y-%m-%d')
+    post_name = item.title.split(%r{ |!|/|:|&|-|$|,}).map do |i|
+        i.downcase if i != ''
+    end.compact.join('-')
+    name = "#{formatted_date}-#{post_name}"
+    
+    header = {
+        'layout' => 'post',
+        'title' => item.title,
+        'subtitle'=> item.itunes_subtitle,
+        'description' => item.summary,
+        'image' => item.itunes_image,
+        'pubdate' => formatted_date,
+        'item' => item,
+        'media' => item.enclosure_url,
+        'duration'=> item.itunes_duration
+
+        }
+
+    FileUtils.mkdir_p("_posts")
+    File.open("_posts/#{name}.html", "w") do |f|
+        f.puts header.to_yaml
+        f.puts "---\n\n"
+    end
+end
